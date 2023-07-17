10f3610a3dfb0561756065d1b944832d12261849
이찬희 <ych0716@naver.com>
2023-07-17 13:30:44 +0900

    init commit

---
+++ src/test/java/com/ssafy/withview/WithviewApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ssafy.withview;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WithviewApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
