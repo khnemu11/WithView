0812a66276147cc5be37f2280969311b937cd9f2
이찬희 <ych0716@naver.com>
2023-07-21 15:57:23 +0900

    [#04] deploy : jenkins build test

--- src/main/java/com/ssafy/withview/controller/WebRestController.java
+++ src/main/java/com/ssafy/withview/controller/WebRestController.java
@@ -4,18 +4,18 @@ import java.util.*;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.PropertySource;
-import org.springframework.core.env.Environment;
+// import org.springframework.core.env.Environment;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import lombok.RequiredArgsConstructor;
+// import lombok.RequiredArgsConstructor;
 
 @RestController
-@RequiredArgsConstructor
+// @RequiredArgsConstructor
 @PropertySource(value = "application.yml")
 public class WebRestController {
-	private final Environment env;
+	// private final Environment env;
 
 	// @GetMapping("/profile")
 	// public String getProfile(){
