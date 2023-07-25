31d552bb37626a21450840201d70c8f4d2880930
이찬희 <ych0716@naver.com>
2023-07-25 14:52:41 +0900

    [#229] deploy : nginx deploy test

--- src/main/java/com/ssafy/withview/WithviewApplication.java
+++ src/main/java/com/ssafy/withview/WithviewApplication.java
@@ -3,12 +3,13 @@ package com.ssafy.withview;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-@SpringBootApplication(exclude = SecurityAutoConfiguration.class)
-public class WithviewApplication {
-
+@SpringBootApplication
+public class WithviewApplication implements WebMvcConfigurer {
 	public static void main(String[] args) {
 		SpringApplication.run(WithviewApplication.class, args);
 	}
-
 }
