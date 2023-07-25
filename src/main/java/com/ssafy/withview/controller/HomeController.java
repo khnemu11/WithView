99246c31c1e2a70d2242304ca40986ba6d1f78b8
이찬희 <ych0716@naver.com>
2023-07-25 16:11:56 +0900

    [#229] deploy : nginx deploy test

--- src/main/java/com/ssafy/withview/controller/HomeController.java
+++ src/main/java/com/ssafy/withview/controller/HomeController.java
@@ -1,15 +1,19 @@
 package com.ssafy.withview.controller;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class HomeController {
 
-	@RequestMapping
+	@ResponseBody
 	@GetMapping("/api/tmp")
-	public String index() {
-		return "index";
+	public ResponseEntity<String> index() {
+		System.out.println("hello");
+		return new ResponseEntity<String>("hello", HttpStatus.OK);
 	}
 }
