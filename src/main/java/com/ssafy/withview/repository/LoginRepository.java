62f69ff541a69248d969a86e3d1f5d44f413b1fc
Hc-5514 <tbd05050@naver.com>
2023-07-27 11:50:20 +0900

    [#170] feat : LoginRepository 작성
    로그인 id로 로그인 정보 가져오기
    
    20230727 이호찬

--- src/main/java/com/ssafy/withview/repository/LoginRepository.java
+++ src/main/java/com/ssafy/withview/repository/LoginRepository.java
@@ -7,6 +7,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import com.ssafy.withview.repository.entity.LoginEntity;
 
 public interface LoginRepository extends JpaRepository<LoginEntity, Integer> {
-
 	Optional<LoginEntity> findById(String id);
 }
