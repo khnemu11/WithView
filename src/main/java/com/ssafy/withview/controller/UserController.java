81fde82d581471593900537bead0c565e382d006
이호찬 <tbd05050@naver.com>
2023-07-27 12:52:35 +0900

    [#135] 로그인 기능 구현

---
+++ src/main/java/com/ssafy/withview/controller/UserController.java
@@ -0,0 +1,87 @@
+package com.ssafy.withview.controller;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.annotation.Secured;
+import org.springframework.security.core.Authentication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.ssafy.withview.repository.dto.JwtDto;
+import com.ssafy.withview.repository.dto.LoginDto;
+import com.ssafy.withview.service.JwtService;
+import com.ssafy.withview.service.LoginService;
+import com.ssafy.withview.service.UserService;
+
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
+@RestController
+@RequestMapping("/api/users")
+public class UserController {
+
+	@Autowired
+	private LoginService loginService;
+
+	@Autowired
+	private JwtService jwtService;
+
+	@Autowired
+	private UserService userService;
+
+	private static final Logger logger = LoggerFactory.getLogger(UserController.class);
+
+	@PostMapping("")
+	public LoginDto join(@RequestBody LoginDto loginDto) {
+		logger.info("UserController: 회원가입 진행");
+		logger.info("loginDto: " + loginDto.getId() + ", " + loginDto.getPassword());
+		return userService.join(loginDto);
+	}
+
+	@PostMapping("/login")
+	public ResponseEntity<Map<String, Object>> login2(@RequestBody LoginDto loginDto) {
+		logger.info("UserController: 로그인 진행");
+		Map<String, Object> resultMap = new HashMap<>();
+		HttpStatus status = null;
+		try {
+			Authentication authentication = loginService.login(loginDto);
+			if (authentication != null) {
+				JwtDto jwtDto = jwtService.generateToken2(authentication);
+				logger.info("UserController: 로그인 성공");
+				logger.info("[JWT] AccessToken: " + jwtDto.getAccessToken()
+					+ ", RefreshToken: " + jwtDto.getRefreshToken());
+				resultMap.put("success", true);
+				resultMap.put("JWT", jwtDto);
+				status = HttpStatus.CREATED;
+			} else {
+				resultMap.put("success", false);
+				status = HttpStatus.ACCEPTED;
+			}
+		} catch (Exception e) {
+			logger.error("UserController: 로그인 실패", e);
+			resultMap.put("success", false);
+			status = HttpStatus.INTERNAL_SERVER_ERROR;
+		}
+		return new ResponseEntity<>(resultMap, status);
+	}
+
+	@GetMapping("/test")
+	public String test() {
+		return "유저";
+	}
+
+	@Secured("ROLE_ADMIN")
+	@GetMapping("/test2")
+	public String test2() {
+		return "관리자";
+	}
+}
---
+++ src/main/java/com/ssafy/withview/repository/dto/JwtDto.java
@@ -0,0 +1,14 @@
+package com.ssafy.withview.repository.dto;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Builder
+public class JwtDto {
+	private String grantType;
+	private String accessToken;
+	private String refreshToken;
+}
