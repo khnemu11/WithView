cce2f62a1f5956a21f03de22120fa8f6f69c2893
Hc-5514 <tbd05050@naver.com>
2023-07-31 17:51:51 +0900

    [#135, #275] feat : 로그인, 회원가입 기능 구현
    로그인, 회원가입 관련 Controller, Service, Dto, Entity 수정
    
    20230731 이호찬

--- src/main/java/com/ssafy/withview/repository/UserRepository.java
+++ src/main/java/com/ssafy/withview/repository/UserRepository.java
@@ -1,14 +1,15 @@
 package com.ssafy.withview.repository;
 
-import com.ssafy.withview.repository.entity.ServerEntity;
-import com.ssafy.withview.repository.entity.UserEntity;
-import com.ssafy.withview.repository.entity.UserServerEntity;
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.List;
+import com.ssafy.withview.repository.entity.UserEntity;
 
 @Repository
-public interface UserRepository extends JpaRepository<UserEntity,Long> {
-    public UserEntity findBySeq(long seq);
+public interface UserRepository extends JpaRepository<UserEntity, Long> {
+	public UserEntity findBySeq(long seq);
+
+	Optional<UserEntity> findById(String id);
 }
--- src/main/java/com/ssafy/withview/service/UserService.java
+++ src/main/java/com/ssafy/withview/service/UserService.java
@@ -1,38 +1,44 @@
 package com.ssafy.withview.service;
 
-import com.ssafy.withview.repository.LoginRepository;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import com.ssafy.withview.repository.LoginRepository;
 import com.ssafy.withview.repository.UserRepository;
-import com.ssafy.withview.repository.dto.LoginDto;
+import com.ssafy.withview.repository.dto.JoinDto;
 import com.ssafy.withview.repository.entity.LoginEntity;
+import com.ssafy.withview.repository.entity.UserEntity;
+
+import lombok.RequiredArgsConstructor;
 
 @Service
+@RequiredArgsConstructor
 public class UserService {
 
-	@Autowired
-	LoginRepository loginRepository;
+	private final UserRepository userRepository;
+	private final LoginRepository loginRepository;
 
 	// 회원가입 시, login, users 테이블에 동시 저장
 	// 현재는 login 테이블에만 저장
 	// 유효성 검사 로직도 추가 예정
 	@Transactional
-	public LoginDto join(LoginDto loginDto) {
+	public Long join(JoinDto joinDto) {
 		BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
 
-		LoginEntity loginEntity = loginRepository.save(LoginEntity.builder()
-			.id(loginDto.getId())
-			.password(encoder.encode(loginDto.getPassword()))
-			.roles("ROLE_USER")
+		UserEntity userEntity = userRepository.save(UserEntity.builder()
+			.id(joinDto.getId())
+			.email(joinDto.getEmail())
+			.nickname(joinDto.getNickname())
 			.build());
 
-		return LoginDto.builder()
-			.id(loginEntity.getId())
-			.password(loginEntity.getPassword())
-			.roles(loginEntity.getRoles())
-			.build();
+		LoginEntity loginEntity = loginRepository.save(LoginEntity.builder()
+			.id(userEntity.getId())
+			.password(encoder.encode(joinDto.getPassword()))
+			.roles("ROLE_USER")
+			.user_seq(userEntity.getSeq())
+			.build());
+
+		return loginEntity.getSeq();
 	}
 }
