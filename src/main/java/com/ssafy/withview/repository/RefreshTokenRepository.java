9ea66e8c8adf2d1bb16f5f412211681cab5c62dc
SuHyun Kim <khnemu@naver.com>
2023-07-31 17:11:06 +0900

    [#1] refact : BE 폴더 생성
    230731 김수현

--- src/main/java/com/ssafy/withview/repository/RefreshTokenRepository.java
+++
@@ -1,9 +0,0 @@
-package com.ssafy.withview.repository;
-
-import org.springframework.data.repository.CrudRepository;
-
-import com.ssafy.withview.repository.entity.RefreshTokenEntity;
-
-public interface RefreshTokenRepository extends CrudRepository<RefreshTokenEntity, String> {
-
-}
