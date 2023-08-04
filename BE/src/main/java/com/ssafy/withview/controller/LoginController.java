45c5143af3ec9122475e1e3ac457bdb1ab340cbc
Hc-5514 <tbd05050@naver.com>
2023-08-04 15:35:01 +0900

    [#135] fix : 로그인 - RefreshToken Cookie 수정
    RefreshToken을 HttpOnly, Secure 설정한 Cookie에 담아서 전송 테스트
    
    230804 이호찬

--- BE/src/main/java/com/ssafy/withview/controller/LoginController.java
+++ BE/src/main/java/com/ssafy/withview/controller/LoginController.java
@@ -62,7 +62,7 @@ public class LoginController {
 				ResponseCookie cookie = ResponseCookie.from("RefreshToken", jwtDto.getRefreshToken())
 					.path("/")
 					.sameSite("None")
-					.httpOnly(false)
+					.httpOnly(true)
 					.secure(true)
 					.maxAge(60)
 					.build();
