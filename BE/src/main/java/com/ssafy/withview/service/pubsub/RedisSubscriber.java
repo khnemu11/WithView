5c331bb43250ef801bacae262d2b52d65723cf7b
이찬희 <ych0716@naver.com>
2023-08-07 09:39:50 +0900

    [#336] etc: 간단한 오타 수정
    230807 이찬희

--- BE/src/main/java/com/ssafy/withview/service/pubsub/RedisSubscriber.java
+++ BE/src/main/java/com/ssafy/withview/service/pubsub/RedisSubscriber.java
@@ -1,12 +1,12 @@
 package com.ssafy.withview.service.pubsub;
 
-import com.ssafy.withview.dto.FriendsChatMessageDto;
 import org.springframework.messaging.simp.SimpMessageSendingOperations;
 import org.springframework.stereotype.Service;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.ssafy.withview.dto.ChannelValueDto;
 import com.ssafy.withview.dto.ChannelChatMessageDto;
+import com.ssafy.withview.dto.ChannelValueDto;
+import com.ssafy.withview.dto.FriendsChatMessageDto;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -27,7 +27,7 @@ public class RedisSubscriber {
 		try {
 			// ChatMessageDTO 객체로 매핑
 			ChannelChatMessageDto chatMessage = objectMapper.readValue(publishMessage, ChannelChatMessageDto.class);
-//			log.info("{}번 채널 채팅 전송, 전송자 {}, 내용 {}", chatMessage.getChannelSeq(), chatMessage.getUserSeq(), chatMessage.getMessage());
+			// log.info("{}번 채널 채팅 전송, 전송자 {}, 내용 {}", chatMessage.getChannelSeq(), chatMessage.getUserSeq(), chatMessage.getMessage());
 			messagingTemplate.convertAndSend("/api/sub/chat/channel/" + chatMessage.getChannelSeq(),
 				chatMessage);
 		} catch (Exception e) {
@@ -39,7 +39,7 @@ public class RedisSubscriber {
 		try {
 			// ChatMessageDTO 객체로 매핑
 			FriendsChatMessageDto chatMessage = objectMapper.readValue(publishMessage, FriendsChatMessageDto.class);
-//			log.info("{}번 친구 채팅 전송, 전송자 {}, 내용 {}", chatMessage.getFriendsChatSeq(), chatMessage.getSendUserSeq(), chatMessage.getMessage());
+			// log.info("{}번 친구 채팅 전송, 전송자 {}, 내용 {}", chatMessage.getFriendsChatSeq(), chatMessage.getSendUserSeq(), chatMessage.getMessage());
 			messagingTemplate.convertAndSend("/api/sub/chat/friends/" + chatMessage.getFriendsChatSeq(),
 				chatMessage);
 		} catch (Exception e) {
@@ -50,7 +50,7 @@ public class RedisSubscriber {
 	public void sendChannelValue(String publishMessage) {
 		try {
 			ChannelValueDto channelValue = objectMapper.readValue(publishMessage, ChannelValueDto.class);
-//			log.info("{}번 서버 인원 변경 전송", channelValue.getServerSeq());
+			// log.info("{}번 서버 인원 변경 전송", channelValue.getServerSeq());
 			messagingTemplate.convertAndSend("/api/sub/server/" + channelValue.getServerSeq(), channelValue);
 		} catch (Exception e) {
 			log.error("Exception {}", e);
