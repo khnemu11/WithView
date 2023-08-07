c0a039b07ae9865e313becb0565daf1f7faba5f4
Hc-5514 <tbd05050@naver.com>
2023-08-07 10:46:32 +0900

    [#135] deploy : 서버 배포용 SecurityConfig 설정
    JWT 로직이 포함이 안된 SecurityConfig 로 변경
    
    20230807 이호찬

--- BE/src/main/java/com/ssafy/withview/config/DevSecurityConfig.java
+++ BE/src/main/java/com/ssafy/withview/config/DevSecurityConfig.java
@@ -1,7 +1,9 @@
 package com.ssafy.withview.config;
 
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
@@ -10,8 +12,8 @@ import org.springframework.web.filter.CorsFilter;
 
 import lombok.RequiredArgsConstructor;
 
-// @Configuration
-// @EnableWebSecurity
+@Configuration
+@EnableWebSecurity
 @RequiredArgsConstructor
 public class DevSecurityConfig {
 
@@ -20,7 +22,7 @@ public class DevSecurityConfig {
 	@Bean
 	public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
 		http.csrf().disable();
-		
+
 		http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
 			.and()
 			.formLogin().disable()
--- BE/src/main/java/com/ssafy/withview/config/SecurityConfig.java
+++ BE/src/main/java/com/ssafy/withview/config/SecurityConfig.java
@@ -1,10 +1,7 @@
 package com.ssafy.withview.config;
 
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
@@ -18,9 +15,9 @@ import com.ssafy.withview.service.JwtService;
 
 import lombok.RequiredArgsConstructor;
 
-@Configuration
-@EnableWebSecurity
-@EnableGlobalMethodSecurity(securedEnabled = true)
+// @Configuration
+// @EnableWebSecurity
+// @EnableGlobalMethodSecurity(securedEnabled = true)
 @RequiredArgsConstructor
 public class SecurityConfig {
 
