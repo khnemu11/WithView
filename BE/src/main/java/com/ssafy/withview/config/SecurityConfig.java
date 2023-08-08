5136ab1bc96f62bf5fac2db5c18fa4ecb62c1f30
Hc-5514 <tbd05050@naver.com>
2023-08-08 17:37:41 +0900

    [#135] setting : SecurityConfig 변경
    아이디 중복 검사, 이메일 인증 관련 로직들 JWT 예외 설정
    
    20230808 이호찬

--- BE/src/main/java/com/ssafy/withview/config/SecurityConfig.java
+++ BE/src/main/java/com/ssafy/withview/config/SecurityConfig.java
@@ -52,7 +52,9 @@ public class SecurityConfig {
 		http.authorizeRequests()
 			.antMatchers("/api/login/login").permitAll()
 			.antMatchers("/api/users").permitAll()
-			.antMatchers("/api/login/cookie").permitAll()
+			.antMatchers("/api/login/check-id").permitAll()
+			.antMatchers("/api/login/email/validate").permitAll()
+			.antMatchers("/api/login/email/authenticate").permitAll()
 			.anyRequest().authenticated();
 
 		return http.build();
