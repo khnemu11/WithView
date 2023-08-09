f0842cd6a7dfd01520c73c62d7f859f7abfbec34
Hc-5514 <tbd05050@naver.com>
2023-08-09 17:02:58 +0900

    [#135] feat : AccessToken 및 RefreshToken 저장 시간 변경
    AccessToken (2분 -> 4시간)
    RefreshToken (5분 -> 24시간)
    
    20230809 이호찬

--- BE/src/main/java/com/ssafy/withview/config/JwtProperties.java
+++ BE/src/main/java/com/ssafy/withview/config/JwtProperties.java
@@ -2,8 +2,8 @@ package com.ssafy.withview.config;
 
 public interface JwtProperties {
 	String SECRET_KEY = "S09P12D208JwtSecretKeyS09P12D208JwtSecretKeyS09P12D208JwtSecretKeyS09P12D208JwtSecretKeyCrazySSAFYMonsterSecurityByHc5514";
-	Integer EXPIRATION_TIME_ACCESS = 1000 * 60 * 2; // 2분
-	Integer EXPIRATION_TIME_REFRESH = 1000 * 60 * 5; // 5분
+	Integer EXPIRATION_TIME_ACCESS = 1000 * 60 * 60 * 4; // 4시간
+	Integer EXPIRATION_TIME_REFRESH = 1000 * 60 * 60 * 24; // 24시간
 	String TOKEN_PREFIX = "Bearer ";
 	String HEADER_STRING_ACCESS = "Authorization";
 }
