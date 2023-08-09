76029473a819b2532902e5fc405254c351e2291c
Hc-5514 <tbd05050@naver.com>
2023-08-09 11:22:49 +0900

    [#135] feat : RefreshToken 만료시간 변경
    20분 -> 5분 수정
    
    20230809 이호찬

--- BE/src/main/java/com/ssafy/withview/entity/RefreshTokenEntity.java
+++ BE/src/main/java/com/ssafy/withview/entity/RefreshTokenEntity.java
@@ -6,7 +6,7 @@ import org.springframework.data.redis.core.RedisHash;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 
-@RedisHash(value = "refreshToken", timeToLive = 1200)
+@RedisHash(value = "refreshToken", timeToLive = 300)
 @Getter
 @AllArgsConstructor
 public class RefreshTokenEntity {
