5535000dbd37bd8137ced8ffbdba64a86e68c861
이호찬 <tbd05050@naver.com>
2023-08-11 09:59:30 +0900

    ## 어떤 이유로 MR를 하셨나요?
    * [x] feature 병합(feature issue #를 남겨주세요)
    * [x] 버그 수정(아래에 issue #를 남겨주세요)
    * [ ] 코드 개선
    * [ ] 기타(아래에 자세한 내용 기입해주세요)
    
    ## 세부 내용 - 왜 해당 MR이 필요한지 자세하게 설명해주세요
    
    
    ## MR하기 전에 확인해주세요
    
    * [x] 정상적으로 빌드가 진행되나요?
    * [x] dev브랜치와 자신의 feat브랜치를 로컬에서 병합을 해보셨나요 ?

--- BE/src/main/java/com/ssafy/withview/WithviewApplication.java
+++ BE/src/main/java/com/ssafy/withview/WithviewApplication.java
@@ -3,9 +3,11 @@ package com.ssafy.withview;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.data.mongodb.config.EnableMongoAuditing;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @EnableJpaAuditing
+@EnableMongoAuditing
 @SpringBootApplication()
 public class WithviewApplication implements WebMvcConfigurer {
 	public static void main(String[] args) {
---
+++ BE/src/main/java/com/ssafy/withview/dto/PresetDto.java
@@ -0,0 +1,24 @@
+package com.ssafy.withview.dto;
+
+import java.time.LocalDateTime;
+
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Builder
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+public class PresetDto {
+	private String id;
+	private Long userSeq;
+	private String presetName;
+	private String presetImgSearchName;
+	private String stage;
+	private LocalDateTime registerTime;
+}
