cb27c75f277b77e72f84eae4cc26f99ccfc8783a
우주연 <dnwndus01@naver.com>
2023-08-16 09:21:57 +0900

    [#537] feat : 창작마당 구현 검색부분 제외

--- FE/D208-front-master/src/routes/board.jsx
+++ FE/D208-front-master/src/routes/board.jsx
@@ -1,38 +1,260 @@
-import React from 'react'
-import ServerOptions from './components/serveroptions'
-import { useSelector } from 'react-redux';
-import { useEffect,useState } from 'react';
-
+import React from "react";
+import ServerOptions from "./components/serveroptions";
+import { useSelector } from "react-redux";
+import { useEffect, useState } from "react";
+import "../css/board.css";
+import axiosInstance from "./axiosinstance";
+import { useNavigate } from "react-router";
 function Board() {
-    
-    const profileNickname = useSelector((state) => state.user.nickname);
-    const [profileImage, setProfileImage] = useState(null);
-    const profileImageURL = useSelector((state) => state.user.profile);
-    const profileImageUrl = `https://dm51j1y1p1ekp.cloudfront.net/profile/${profileImageURL}`;
-    
+  const profileNickname = useSelector((state) => state.user.nickname);
+  const userPk = useSelector((state)=>state.user.seq)
+  const token = useSelector((state)=>state.token)
+  const [profileImage, setProfileImage] = useState(null);
+  const [isModalActive, setIsModalActive] = useState(false);
+  const profileImageURL = useSelector((state) => state.user.profile);
+  const profileImageUrl = `https://dm51j1y1p1ekp.cloudfront.net/profile/${profileImageURL}`;
+  const [title, setTitle] = useState("")
+  const [content, setContent] = useState("")
+  const [presetList, setPresetList] = useState([])
+  const [presetCard, setPresetCard] = useState([])
+  const [selectedImageId, setSelectedImageId] = useState([]);
+  const navigate = useNavigate()
+  
+  
+  useEffect(() => {
+    // 만약 redux에서 프로필 이미지가 null이면 기본 이미지로 설정
+    if (profileImageURL === null) {
+      setProfileImage("/withView2.png");
+    } else {
+      setProfileImage(profileImageUrl);
+    }
+  }, [profileImageURL]);
 
-    useEffect(() => {
-        // 만약 redux에서 프로필 이미지가 null이면 기본 이미지로 설정
-        if (profileImageURL === null) {
-          setProfileImage("/withView2.png");
-        } else {
-          setProfileImage(profileImageUrl);
-        }
-      }, [profileImageURL]);
+  useEffect(() => {
+    axiosInstance({
+      headers: {
+        "Authorization": `Bearer ${token}`
+      },
+      method: "GET",
+      url: `/preset/${userPk}/list`,  
+    })
+    .then((res)=>{
+      console.log(res.data.PresetListInfo)
+      setPresetList(res.data.PresetListInfo)
+    })
+    .catch((err)=>{
+      console.log(err)
+    })
+  }, []);
+
+  useEffect(() => {
+    axiosInstance({
+      headers: {
+        "Authorization": `Bearer ${token}`
+      },
+      method: "GET",
+      url: `/board`,  
+    })
+    .then((res)=>{
+      console.log(res.data)
+      setPresetCard(res.data.BoardListInfo)
+      
+    })
+    .catch((err)=>{
+      console.log(err)
+    })
+  }, []);
+
+  function WritePost(){
+    console.log(selectedImageId[0])
+    axiosInstance({
+      headers: {
+        "Authorization": `Bearer ${token}`
+      },
+      method: "POST",
+      url: `/board`, 
+      data : {
+        userSeq : userPk,
+        title : title,
+        content : content,
+        presetId : selectedImageId[0],
+      }
+    })
+    .then((res)=>{
+      console.log(res.data)
+      setTitle('')
+      setContent('')
+      setSelectedImageId([])
+      setIsModalActive(false)
+    
+    })
+    .catch((err)=>{
+      console.log(err)
+    })
+  }
+
+
+  const handleImageClick = (name) => {
+    const isSelected = selectedImageId.includes(name);
+    
+    if (isSelected) {
+      setSelectedImageId(selectedImageId.filter((imageId) => imageId !== name));
+    } else {
+      setSelectedImageId([name]);
+    }
+    console.log(selectedImageId)
+  };
+
+  
+  
+  const PresetImages = presetList.map((el) => {
+    const presetUrl = `https://dm51j1y1p1ekp.cloudfront.net/preset/${el.presetImgSearchName}`;
+    const isSelected = selectedImageId.includes(el.id);
+    return (
+      <div key = {el.id}>
+        <p>{el.presetName}</p>
+        <div className={`board_modal_image is-21by9`} onClick={()=>handleImageClick(el.id)}>
+
+          <img className={`${isSelected ? 'board_modal_image_selected' : ''}`}src={presetUrl} alt="아오"/>
+          {isSelected && (
+            <i className="fa-solid fa-check fa-bounce fa-5x board_check_icon" style={{color: "#0aeb24"}}></i>
+        )}
+        </div>
+      </div>
+    );
+  });
+
+  const PresetCardImages = presetCard.map((el)=>{
+    // console.log(el)
+    const presetUrl = `https://dm51j1y1p1ekp.cloudfront.net/preset/${el.presetImgSearchName}`
     
     return (
-    
-    <div className="innermain">
-        <ServerOptions
-            profileImage={profileImage}
-            profileNickname={profileNickname}
-        />
-    
-        <hr className="serverOptionsLine_profile" />
+      <div className="board_card card" key={el.boardSeq} onClick={()=>navigate(`/board/${el.boardSeq}`)}>
+        <header className="card-header">
+          <p className="card-header-title">{el.title}</p>
+        </header>
+        <div className="card-content image is-4by3">
+          <img src={presetUrl} alt="없음" />
+        </div>
+        <footer className="card-footer board_card_footer">
+          <p>작성자 : {el.nickname}</p>
+        </footer>
+      </div>
+    )
+  })
+  
 
+  function createContentStart() {
+    setIsModalActive(true)
+    
+    
+  }
+  return (
+    <div className="innermain">
+      <ServerOptions
+        profileImage={profileImage}
+        profileNickname={profileNickname}
+      />
+
+      <hr className="serverOptionsLine_profile" />
+      
+      <div className={`modal ${isModalActive ? "is-active" : ""}`} id="myModal">
+        <div
+          className={`modal-background ${isModalActive ? "is-active" : ""}`}
+          onClick={() => {
+              setContent('')
+              setTitle('')
+              setSelectedImageId([])
+              setIsModalActive(false);
+          }} // 모달 배경 클릭 시 모달 닫기
+        ></div>
+        <div className="modal-card board_modal_card">
+          <header className="modal-card-head">
+            <p className="modal-card-title">창작마당 글쓰기</p>
+            <button
+              className="delete"
+              aria-label="close"
+              onClick={() => {
+                setContent('')
+                setTitle('')
+                setSelectedImageId([])
+                setIsModalActive(false);
+
+              }} // 모달 닫기 버튼 클릭 시 setIsModalActive(false) 호출
+            ></button>
+          </header>
+
+          <section className="modal-card-body">
+            <p>제목</p>
+            <input
+              type="text"
+              className="input"
+              value={title}
+              onChange={(e) => {
+                setTitle(e.target.value)
+              }}
+            />
+            <hr />
+            <p>프리셋</p>
+              {PresetImages}
+            <br />
+            <p>내용</p>
+            <textarea className="textarea is-info" 
+              value={content}
+              placeholder="Info textarea"
+              onChange={(e)=>{
+                setContent(e.target.value)
+              }}
+            >
+            </textarea>
+          </section>
+
+          <footer className="modal-card-foot">
+            <button className="button is-info" onClick={WritePost}>
+              작성
+            </button>
+
+            <button
+              className="button"
+              onClick={() => {
+                setContent('')
+                setTitle('')
+                setSelectedImageId([])
+                setIsModalActive(false);
+              }}
+            >
+              닫기
+            </button>
+          </footer>
+        </div>
+      </div>
+      <div className="board_input">
         
+
+        <i className="fa-solid fa-file-pen board_write" style={{fontSize : "2em"}} onClick={createContentStart}></i>
+        
+        <div>
+          검색
+          <input className="intput" type="text"
+            onChange={(e)=>{
+
+              // const searchCard = presetList.filter((el)=>{
+              //   console.log(el.title)
+              //   return el.title.includes(e.target.value)
+              // })
+              // e.target.value
+              console.log(e.target.value)
+              
+            }}
+          />
+        </div>
+      </div>
+      <div className="board_notice">
+
+        {PresetCardImages}
+      </div>
     </div>
-  )
+  );
 }
 
-export default Board
+export default Board;
---
+++ FE/D208-front-master/src/routes/presetpost.jsx
@@ -0,0 +1,335 @@
+import React from "react";
+import ServerOptions from "./components/serveroptions";
+import { useNavigate, useParams } from "react-router";
+import { useSelector } from "react-redux";
+import { useEffect, useState } from "react";
+import "../css/presetpost.css";
+import axiosInstance from "./axiosinstance";
+
+function Presetpost() {
+  const { seq } = useParams();
+  const navigate = useNavigate();
+  const profileNickname = useSelector((state) => state.user.nickname);
+  const token = useSelector((state) => state.token);
+  const userPk = useSelector((state) => state.user.seq);
+  const [profileImage, setProfileImage] = useState(null);
+  const profileImageURL = useSelector((state) => state.user.profile);
+  const profileImageUrl = `https://dm51j1y1p1ekp.cloudfront.net/profile/${profileImageURL}`;
+  const [postContent, setPostContent] = useState("");
+  const presetPostImageUrl = `https://dm51j1y1p1ekp.cloudfront.net/preset/${postContent.presetImgSearchName}`;
+  const profileImageUrl2 = `https://dm51j1y1p1ekp.cloudfront.net/profile/${postContent.profileImgSearchName}`;
+  const [isModalActive, setIsModalActive] = useState(false);
+  const [selectedImageId, setSelectedImageId] = useState([]);
+  const [title, setTitle] = useState("");
+  const [content, setContent] = useState("");
+  const [modifyButton, setModifyButton] = useState(false);
+  const [presetList, setPresetList] = useState([]);
+
+  useEffect(() => {
+    // 만약 redux에서 프로필 이미지가 null이면 기본 이미지로 설정
+    if (profileImageURL === null) {
+      setProfileImage("/withView2.png");
+    } else {
+      setProfileImage(profileImageUrl);
+    }
+  }, [profileImageURL]);
+
+  // 개별 게시물 get
+  useEffect(() => {
+    axiosInstance({
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+      method: "GET",
+      url: `/board/${seq}`,
+    })
+      .then((res) => {
+        console.log(res.data.BoardInfo);
+        setPostContent(res.data.BoardInfo);
+        if (res.data.BoardInfo.userSeq === userPk){
+            setModifyButton(true)
+        }
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }, []);
+
+  useEffect(() => {
+    axiosInstance({
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+      method: "GET",
+      url: `/preset/${userPk}/list`,
+    })
+      .then((res) => {
+        console.log(res.data.PresetListInfo);
+        setPresetList(res.data.PresetListInfo);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }, []);
+
+  function UpdatePost() {
+    console.log(selectedImageId[0]);
+    axiosInstance({
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+      method: "PUT",
+      url: `/board`,
+      data: {
+        boardSeq: seq,
+        title: title,
+        content: content,
+        presetId: selectedImageId[0],
+      },
+    })
+      .then((res) => {
+        console.log(res.data);
+        alert("수정 완료!");
+        setTitle("");
+        setContent("");
+        setSelectedImageId([]);
+        setIsModalActive(false);
+
+        setPostContent((prevPostContent) => ({
+            ...prevPostContent,
+            title: title,
+            content: content,
+            presetId: selectedImageId[0],
+          }));
+
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }
+
+  const PresetImages = presetList.map((el) => {
+    const presetUrl = `https://dm51j1y1p1ekp.cloudfront.net/preset/${el.presetImgSearchName}`;
+    const isSelected = selectedImageId.includes(el.id);
+    return (
+      <div key={el.id}>
+        <p>{el.presetName}</p>
+        <div
+          className={`board_modal_image is-21by9`}
+          onClick={() => handleImageClick(el.id)}
+        >
+          <img
+            className={`${isSelected ? "board_modal_image_selected" : ""}`}
+            src={presetUrl}
+            alt="아오"
+          />
+          {isSelected && (
+            <i
+              className="fa-solid fa-check fa-bounce fa-5x board_check_icon"
+              style={{ color: "#0aeb24" }}
+            ></i>
+          )}
+        </div>
+      </div>
+    );
+  });
+
+  const handleImageClick = (name) => {
+    const isSelected = selectedImageId.includes(name);
+
+    if (isSelected) {
+      setSelectedImageId(selectedImageId.filter((imageId) => imageId !== name));
+    } else {
+      setSelectedImageId([name]);
+    }
+    console.log(selectedImageId);
+  };
+
+  const UpdateContent = () => {
+    setTitle(postContent.title);
+    setContent(postContent.content);
+    setSelectedImageId([postContent.presetId]);
+    setIsModalActive(true);
+  };
+
+  const DeleteContent = () => {
+    axiosInstance({
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+      method: "DELETE",
+      url: `/board/${seq}`,
+    })
+      .then((res) => {
+        console.log(res.data);
+        alert("삭제완료!");
+        navigate("/board");
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
+
+  async function imageUrlToBlob(url) {
+    try {
+      const response = await fetch(url);
+      const blob = await response.blob();
+      return blob;
+    } catch (error) {
+      console.error("이미지를 변환하는 동안 오류가 발생했습니다.", error);
+      throw error; // 에러를 다시 던져서 상위 함수로 전달
+    }
+  }
+
+  // 이미지 URL을 바로 파일로 만들어 axios 요청을 보내는 함수
+  async function makeFileImg() {
+    try {
+      // 이미지 URL을 Blob으로 변환
+      const blob = await imageUrlToBlob(presetPostImageUrl);
+
+      // Blob을 File로 변환
+      const file = new File([blob], "presetImage.png", {
+        type: "image/png",
+      });
+
+      // axios 요청 보내기
+      axiosInstance({
+        headers: {
+          "Content-Type": "multipart/form-data",
+          Authorization: `Bearer ${token}`,
+        },
+        method: "POST",
+        url: `/preset`,
+        data: {
+          userSeq: userPk,
+          presetName: postContent.title,
+          stage: postContent.stage,
+          file: file,
+        },
+      })
+        .then((res) => {
+          console.log(res.data);
+          alert("저장완료 !!");
+        })
+        .catch((err) => {
+          console.log(err);
+        });
+    } catch (error) {
+      console.error("이미지를 변환하는 동안 오류가 발생했습니다.", error);
+    }
+  }
+
+  return (
+    <div className="innermain">
+      <ServerOptions
+        profileImage={profileImage}
+        profileNickname={profileNickname}
+      />
+
+      <hr className="serverOptionsLine_profile" />
+
+      <div className="card presetpost_card">
+        <div className="card-image">
+          <figure className="image is-21by9">
+            <img src={presetPostImageUrl} alt="Placeholder image" />
+          </figure>
+        </div>
+        <div className="card-content">
+          <div className="media">
+            <div className="media-left" style={{ marginRight: "25px" }}>
+              <figure className="image is-96x96">
+                <img src={profileImageUrl2} alt="Placeholder image" />
+              </figure>
+            </div>
+
+            <div className="media-content presetpost_media-content">
+              <p className="title is-2">{postContent.title}</p>
+              <p className="subtitle is-5">작성자 : {postContent.nickname}</p>
+            </div>
+          </div>
+
+          <div className="content" >
+            <blockquote>{postContent.content}</blockquote>
+            <br />
+            <p className="subtitle is-5">{postContent.registerTime}</p>
+          </div>
+
+          <div className="presetpost_buttons">
+            
+            <i className="fa-solid fa-chevron-left fa-3x presetpost_icon" onClick={() => navigate("/board")}></i>
+            
+            <i className="fa-solid fa-download fa-5x presetpost_icon" onClick={makeFileImg}></i>
+            <div className={`${modifyButton ? "" : "is-hidden"}`}>
+                <i className="fa-solid fa-pen-to-square fa-3x presetpost_icon" onClick={UpdateContent} style={{marginRight : "20px"}}></i>
+                <i className="fa-solid fa-trash-can fa-3x presetpost_icon" onClick={DeleteContent} style={{marginLeft : "20px"}}></i>
+              
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div className={`modal ${isModalActive ? "is-active" : ""}`} id="myModal">
+        <div
+          className={`modal-background ${isModalActive ? "is-active" : ""}`}
+          onClick={() => {
+            setIsModalActive(false);
+          }} // 모달 배경 클릭 시 모달 닫기
+        ></div>
+        <div className="modal-card board_modal_card">
+          <header className="modal-card-head">
+            <p className="modal-card-title">창작마당 수정</p>
+            <button
+              className="delete"
+              aria-label="close"
+              onClick={() => {
+                setIsModalActive(false);
+              }} // 모달 닫기 버튼 클릭 시 setIsModalActive(false) 호출
+            ></button>
+          </header>
+
+          <section className="modal-card-body">
+            <p>제목</p>
+            <input
+              type="text"
+              className="input"
+              value={title}
+              onChange={(e) => {
+                setTitle(e.target.value);
+              }}
+            />
+            <hr />
+            <p>프리셋</p>
+            {PresetImages}
+            <br />
+            <p>내용</p>
+            <textarea
+              className="textarea is-info"
+              placeholder="Info textarea"
+              value={content}
+              onChange={(e) => {
+                setContent(e.target.value);
+              }}
+            ></textarea>
+          </section>
+
+          <footer className="modal-card-foot">
+            <button className="button is-info" onClick={UpdatePost}>
+              작성
+            </button>
+
+            <button
+              className="button"
+              onClick={() => {
+                setIsModalActive(false);
+              }}
+            >
+              취소
+            </button>
+          </footer>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Presetpost;
